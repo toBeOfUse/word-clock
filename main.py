a783c616e687f11e95fe32948d73158acae672f5
Mitch J <mjacovet@kent.edu>
2025-08-04 09:37:03 -0400

    add parse_all script

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from word-clock!")
-
-
-if __name__ == "__main__":
-    main()
---
+++ parse_all.py
@@ -0,0 +1,11 @@
+from parse_font import parse_font
+from parse_words import parse_words
+
+
+def main():
+    parse_font()
+    parse_words()
+
+
+if __name__ == "__main__":
+    main()
